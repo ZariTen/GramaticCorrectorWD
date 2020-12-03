4abbdc5aa16134c82612b8a1130d3b54be1cdbdd
Wheados <Wheadosz@hotmail.com>
2020-12-03 19:39:53 -0300

    removed templates

--- src/example.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-void print_hello(){
-    
-    std::cout << "Hello world" << std::endl;
-    std::cout << "This is a test" << std::endl;
-    
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <example.hpp>
 
 int main(int argc, char* argv[]){
-        print_hello();
         
         return 0;
 }
