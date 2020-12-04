f0e69a03cadf6130468092700eedbb37e6d39853
Wheados <Wheadosz@hotmail.com>
2020-12-04 18:17:32 -0300

    first works

---
+++ include/dicionario.h
@@ -0,0 +1,26 @@
+#ifndef DICT_HPP
+#define DICT_HPP
+#include <unordered_set>
+#include <string>
+#include <levenshtein.h>
+
+struct dicionario{
+    
+    void lerArquivo(std::string nome){
+
+
+    }
+
+    bool verificarExistencia(std::string palavra){
+
+        return false;
+    }
+
+    void sugerirPalavras(std::string palavra){
+
+
+    }
+
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
+#include <dicionario.h>
 
 int main(int argc, char* argv[]){
-        
+        //argv[1] = dicionario
+        //argv[2] = texto
+
         return 0;
 }
